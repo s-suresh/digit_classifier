081ce663daaca3852b71050096b58260458ba87d
s-suresh <87108396+s-suresh@users.noreply.github.com>
2025-06-26 12:25:45 +0530

    Update app.py
    changed the model from model.keras to model_finetuned.h5

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 # Load trained model
 @st.cache_resource()
 def load_model():
-    return tf.keras.models.load_model("model.keras")
+    return tf.keras.models.load_model("model_finetuned.h5")
 
 model = load_model()
 
